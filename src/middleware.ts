4ee5f21debf73e7567d93c175fcb9bb7b4f2154a
DESKTOP-D30EHJS\serpa <serpantino@msn.com>
2023-08-03 12:41:14 +0100

    Added auth check to /dashboard child routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware();
 
 export const config = {
-  matcher: ["/dashboard", "/(api|trpc)(.*)"],
+  matcher: [
+    "/dashboard/home",
+    "/(api|trpc)(.*)",
+    "/dashboard/analytics",
+    "/dashboard/settings",
+  ],
 };
