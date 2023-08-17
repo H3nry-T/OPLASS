734720865286b3c36016ee27eed04f9034ee0605
H3nry-T <ht3drex@gmail.com>
2023-08-17 13:38:53 +0100

    some styling on the menu

--- src/components/DashboardNavDialog.tsx
+++ src/components/DashboardNavDialog.tsx
@@ -22,11 +22,18 @@ export default function DashBoardDialogNav() {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-gray-700/70 data-[state=open]:animate-overlayShow fixed inset-0" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-gray-950  p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-            Edit profile
+          <Dialog.Title className="m-0 text-2xl font-medium ">
+            Nav menu
           </Dialog.Title>
-          <Dialog.Description className="mt-[10px] mb-5 text-[15px] leading-normal">
-            Make changes to your profile here. Click save when you're done.
+          <Dialog.Description
+            className="mt-[10px] mb-5 text-lg leading-normal"
+            asChild
+          >
+            <ul>
+              <li>home</li>
+              <li>analytics</li>
+              <li>settings</li>
+            </ul>
           </Dialog.Description>
 
           <Dialog.Close className="hover:bg-gray-800">
